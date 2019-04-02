694df4f6872ed9eb496a31bb6f16d6feaec50921
Luís Barbosa <luis.barbosa@360imprimir.pt>
2019-04-02 09:38:44 +0100

    Added projects

---
+++ test/PseudoLocalizerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PseudoLocalizer.Tests
+{
+    public class PseudoLocalizerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
