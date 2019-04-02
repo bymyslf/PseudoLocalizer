2c73e5576cd92a86f0c0b1d0c5bf62704ecbb6a0
Luís Barbosa <luis.barbosa@360imprimir.pt>
2019-04-02 09:45:06 +0100

    Added text transformation

--- test/PseudoLocalizerTests.cs
+++ test/PseudoLocalizerTests.cs
@@ -1,14 +1,34 @@
-using System;
-using Xunit;
+﻿using Xunit;
 
 namespace PseudoLocalizer.Tests
 {
     public class PseudoLocalizerTests
     {
         [Fact]
-        public void Test1()
+        public void PseudoLocalize_TextIsNull_ReturnsNull()
         {
+            var result = PseudoLocalizer.PseudoLocalize(null);
 
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void PseudoLocalize_TextIsEmpty_ReturnsEmptyString()
+        {
+            var result = PseudoLocalizer.PseudoLocalize(string.Empty);
+
+            Assert.Equal(string.Empty, result);
+        }
+
+        [Fact]
+        public void PseudoLocalize_TextIsNotNullOrEmpty_ReturnsTransformedText()
+        {
+            var text = "TXT";
+            var expected = "[ŢẊŢ]";
+
+            var result = PseudoLocalizer.PseudoLocalize(text);
+
+            Assert.Equal(expected, result);
         }
     }
 }
